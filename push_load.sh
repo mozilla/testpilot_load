6ba0afda099aacff35ae86660c57ee31deb0adb4
Daniel Thorn <daniel@relud.com>
2016-04-22 11:21:44 -0700

    Update push_load.sh

--- push_load.sh
+++ push_load.sh
@@ -8,7 +8,7 @@
 #               `heka-logs` S3 bucket.
 #
 ############################
-BASE_DIR=/opt/push_load
+BASE_DIR=/opt/testpilot_load
 sudo yum install -y postgresql-libs
 sudo yum install -y $BASE_DIR/rpms/luasandbox-0.10.2-Linux-core.rpm
 sudo ldconfig
@@ -20,10 +20,7 @@ sudo yum install -y $BASE_DIR/rpms/hindsight-0.3.0-Linux.rpm
 # places. The first is ensuring the AWS S3 credentials are correctly configured
 # to reach the `heka-logs` bucket. The second is ensuring the Redshift database
 # settings are correct in the
-# `$BASE_DIR/hindsight/hs_run/output/push_endp_redshift.cfg` file.
+# `$BASE_DIR/hindsight/hs_run/output/testpilot_endp_redshift.cfg` file.
 #
 ############################
 hindsight_cli $BASE_DIR/hindsight/etc/hindsight.cfg
-
-cd $BASE_DIR/report
-lua report.lua
